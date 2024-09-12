4b46b6084d9e9ddc150f74920fb45abf8d63fb50
Le Thien Son <lethienson@Les-MBP-7.STUSGS.RMIT.EDU.VN>
2024-09-12 13:02:54 +0700

    changed screen size to 600x800 (vertical)

--- kernel/framebf.c
+++ kernel/framebf.c
@@ -39,14 +39,14 @@ void framebf_init() {
   mBuf[2] = MBOX_TAG_SETPHYWH; // Set physical width-height
   mBuf[3] = 8;                 // Value size in bytes
   mBuf[4] = 0;                 // REQUEST CODE = 0
-  mBuf[5] = 711;               // Value(width)
-  mBuf[6] = 654;               // Value(height)
+  mBuf[5] = 600;               // Value(width)
+  mBuf[6] = 800;               // Value(height)
 
   mBuf[7] = MBOX_TAG_SETVIRTWH; // Set virtual width-height
   mBuf[8] = 8;
   mBuf[9] = 0;
-  mBuf[10] = 1422;
-  mBuf[11] = 1308;
+  mBuf[10] = 1200;
+  mBuf[11] = 1600;
 
   mBuf[12] = MBOX_TAG_SETVIRTOFF; // Set virtual offset
   mBuf[13] = 8;
