a8e40b29c9d5aef2e552df400fc7591ffce932da
thaobwi <129365706+thaobwi@users.noreply.github.com>
2024-09-24 13:57:30 +0700

    Update shooter.c

--- game/shooter.c
+++ game/shooter.c
@@ -312,9 +312,8 @@ int isGameOver() {
     for (int col = 0; col < COLS; col++) {
         if (rowsOnScreen == 12) {
           drawImage(0,0,myOver, 700,800);
-          draw_string(130, 660, "Enter 'r' to reset the game!", 0xFFFF69B4, 2);
-          draw_string(130, 700, "Enter 'q' to quite the game!", 0xFFFF69B4, 2);
-
+          draw_string_with_background(130, 660, "Enter 'r' to reset the game!", 0xFFFF69B4, 0xFF000000, 2);
+          draw_string_with_background(130, 700, "Enter 'q' to quite the game!", 0xFFFF69B4, 0xFF000000, 2);
             return 1;  // Game over condition met
         }
     }
@@ -422,9 +421,9 @@ void moveShooter() {
       {
         uart_puts("\nGame Paused\n");
         drawImage(0, 0, myPause, 700, 800); // Draw the pause image
-        draw_string(130, 640, "Enter 'c' to continue the game!", 0xFFFF69B4, 2);
-        draw_string(130, 680, "Enter 'q' to quite the game!", 0xFFFF69B4, 2);
-        draw_string(130, 720, "Enter 'r' to reset the game!", 0xFFFF69B4, 2);
+       draw_string_with_background(130, 640, "Enter 'c' to continue the game!", 0xFFFF69B4, 0xFF000000, 2);
+        draw_string_with_background(130, 680, "Enter 'q' to quite the game!", 0xFFFF69B4, 0xFF000000, 2);
+        draw_string_with_background(130, 720, "Enter 'r' to reset the game!", 0xFFFF69B4, 0xFF000000, 2);
 
         isPaused = 1;                       // Set the game to paused
       }
